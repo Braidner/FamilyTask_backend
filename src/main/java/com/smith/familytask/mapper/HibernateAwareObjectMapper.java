b108560cf521e69dfa9c9586d178f7b45fbb8fe9
Nick Smith <goodly92@gmail.com>
2015-03-21 16:08:57 +0300

    Update

--- src/main/java/com/smith/familytask/mapper/HibernateAwareObjectMapper.java
+++ src/main/java/com/smith/familytask/mapper/HibernateAwareObjectMapper.java
@@ -2,10 +2,12 @@ package com.smith.familytask.mapper;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.hibernate4.Hibernate4Module;
+import org.springframework.stereotype.Component;
 
 /**
  * @author smith
  */
+@Component("hibernateMapper")
 public class HibernateAwareObjectMapper extends ObjectMapper {
     public HibernateAwareObjectMapper() {
         super();
